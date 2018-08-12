ffee2e850e8737203fa3607cf5f185a04af819fb
trsiva16 <40989942+trsiva16@users.noreply.github.com>
2018-08-12 22:49:55 +0530

    Add files via upload

---
+++ cardPermutation_2dArray.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class cardPermutation
+    {
+        //public static void Main(string[] args)
+        //{
+        //    ulong[,] arr;
+            
+        //    arr = new ulong[39916800, 12];
+        //    CardPermutation();            
+        //}
+
+        public static void CardPermutation()
+        {
+            long N = 20;
+            long[,] newPermArray, oldPermArray=null;
+            //long[] input = new long[] { 0, 3, 2, 0 };
+            //long Output = 0;
+
+            for (long i = N; i >= 1; i--)
+            {
+                long permutationcountMultiplexer = (N - i) + 1;
+                long row = i < N ? ((oldPermArray.Length / (N - i)) * permutationcountMultiplexer) : permutationcountMultiplexer;
+                long column = permutationcountMultiplexer;
+                newPermArray = new long[row, column];
+                if ((permutationcountMultiplexer - 1) == 0)
+                {
+                    oldPermArray = new long[1, 1];
+                    oldPermArray[0, 0] = i;
+                }
+                else
+                {
+                    oldPermArray = permutations(N, i, permutationcountMultiplexer, newPermArray, oldPermArray);
+                }
+            }
+
+            //for (long m = 0; m < (oldPermArray.Length / N); m++)
+            //{
+            //    bool match = true;
+            //    for (long n = 0; n < N; n++)
+            //    {
+            //        if (input[n] != 0 && input[n] != oldPermArray[m, n])
+            //        {
+            //            match = false;
+            //            break;
+            //        }
+            //    }
+            //    if (match)
+            //    {
+            //        Output += (m + 1);
+            //    }
+            //}
+            Console.WriteLine(oldPermArray.Length);
+            Console.ReadLine();
+        }
+
+
+        public static long[,] permutations(long N, long newPermNo,long permutationcountMultiplexer, long[,] newPermArray, long[,] oldPermArray)
+        {
+            long row = 0, myIndex = 0;
+
+            for(long i=0;i<permutationcountMultiplexer;i++)
+            {
+                for (long j = 0; j < (oldPermArray.Length/(N-newPermNo)); j++)
+                {
+                    for(long m=0;m<permutationcountMultiplexer;m++)
+                    {
+                        if (m == myIndex)
+                        {
+                            newPermArray[row, myIndex] = newPermNo;
+                        }else if(m<myIndex)
+                        {
+                            newPermArray[row, m] = oldPermArray[j, m];
+                        }else if(m>myIndex)
+                        {
+                            newPermArray[row, m] = oldPermArray[j, m - 1];
+                        }
+                    }
+                    row++;
+                }
+                myIndex++;
+            }
+            return newPermArray;
+        }
+
+    } 
+}
---
+++ cardPermutation_jaggedArray.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class cardPermutationjagged
+    {
+        public static void Main(string[] args)
+        {
+            //int[,] arr;            
+            //arr = new int[39916800, 12];
+
+            CardPermutation();            
+        }
+
+        public static void CardPermutation()
+        {
+            ulong[] input = new ulong[] { 1, 2, 3, 4, 5, 6, 7, 8,9,10};
+            ulong Output = 0;
+            ulong N = 10;
+            ulong[][] newPermArray, oldPermArray=null;
+
+            for (ulong i = N; i >= 1; i--)
+            {
+                ulong permutationcountMultiplexer = (N - i) + 1;
+                ulong row = i < N ? (ulong)oldPermArray.Length * permutationcountMultiplexer : permutationcountMultiplexer;
+
+                ulong column = permutationcountMultiplexer;
+
+                newPermArray = new ulong[row][];
+
+                if ((permutationcountMultiplexer - 1) == 0)
+                {
+                    ulong[] coln = new ulong[1];
+                    coln[0] = i;
+                    oldPermArray = new ulong[1][];
+                    oldPermArray[0] = coln;
+                }
+                else
+                {
+                    oldPermArray = permutations(input,ref Output, N, i, permutationcountMultiplexer, newPermArray, oldPermArray);
+                }
+            }
+            Console.WriteLine(Output);
+            Console.ReadLine();
+        }
+
+
+        public static ulong[][] permutations(ulong[] input,ref ulong output, ulong N, ulong newPermNo,ulong permutationcountMultiplexer, ulong[][] newPermArray, ulong[][] oldPermArray)
+        {
+            ulong row = 0, myIndex = 0;
+
+            for(ulong i=0;i<permutationcountMultiplexer;i++)
+            {
+                for (int j = 0; j < oldPermArray.Length; j++)
+                {
+                    ulong[] coln = new ulong[permutationcountMultiplexer];
+
+                    for(ulong m=0;m<permutationcountMultiplexer;m++)
+                    {
+                        if (m == myIndex)
+                        {
+                            coln[m] = newPermNo;
+                        }else if(m<myIndex)
+                        {
+                            coln[m] = oldPermArray[j][m];
+                        }else if(m>myIndex)
+                        {
+                            coln[m] = oldPermArray[j][m - 1];
+                        }
+                    }
+
+                    if ((ulong)input.Length == permutationcountMultiplexer)
+                    {
+                        if(Outp(input,permutationcountMultiplexer,coln))
+                        {
+                            output += (row + 1);
+                        }
+                    }
+
+                    newPermArray[row] = coln;
+                    row++;
+
+
+                }
+                myIndex++;
+            }
+            return newPermArray;
+        }
+
+
+        public static bool Outp(ulong[] input, ulong permutationcountMultiplexer, ulong[] coln)
+        {
+            bool match = true;
+       
+                for (ulong n = 0; n < permutationcountMultiplexer; n++)
+                {
+                    if (input[n] != 0 && input[n] != coln[n])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+            return match;
+        }
+           
+        
+
+
+    } 
+}
